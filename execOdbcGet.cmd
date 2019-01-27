df53566f71a8b6d9fdbc487c5ba4c768a09dbacc
Pic0o <47042955+botsama@users.noreply.github.com>
2019-01-27 05:05:17 -0500

    Add files via upload

---
+++ execOdbcGet.cmd
@@ -0,0 +1,3 @@
+@echo off
+echo Output tree of ODBC registry path to screen.
+PowerShell.exe -ExecutionPolicy Bypass -File getOdbc.ps1
---
+++ getOdbc.ps1
@@ -0,0 +1,11 @@
+﻿# Open registry to check for ODBC / DSN items.
+
+$boxName = $env:COMPUTERNAME
+#$getOldDbOdbc = Get-ItemProperty -Path 'HKLM:\Software\Wow6432Node\ODBC\ODBC.INI\ODBC Data Sources'
+$getDbOdbc = Get-ChildItem -Path 'HKLM:\Software\Wow6432Node\ODBC\ODBC.INI'
+$getDb2Odbc = Get-ChildItem -Path 'HKLM:\Software\ODBC\ODBC.INI'
+
+
+Write-Warning "If no details, very likely no System DSNs defined on $boxName"
+Write-Output $getDbOdbc
+Write-Output $getDb2Odbc
